e7c99c62567520e4ddcefee4b8c2b779ef7d0263
Samuel Cherisme <ssscherisme@gmail.com>
2025-12-09 20:01:55 -0500

    Removed uneeded styles

--- src/pages/pricing/pricing.ts
+++ src/pages/pricing/pricing.ts
@@ -1,15 +1,14 @@
 // pages/pricing/pricing.ts
 
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; // <-- CRITICAL: Import RouterLink here
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [RouterLink], // <-- CRITICAL: Add RouterLink to the imports array
+  imports: [RouterLink],
   templateUrl: './pricing.html',
   styleUrl: './pricing.css'
 })
 export class Pricing {
-  // Component logic goes here
+
 }
